e54efa00a6ec65dd6c7c3311fb1397fbb6b7583c
Eran Deeshon <deeshon.s@circles.asia>
2023-08-14 10:34:11 +0530

    Added routes for dashboard, auth, blog, product, tutorials

--- client-spa/src/App.tsx
+++ client-spa/src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, Routes } from "react-router-dom"
+import Dashboard from './pages/Dashboard'
+import Auth from './pages/Auth'
+import Blog from './pages/Blog'
+import Product from './pages/Product'
+import Tutorials from './pages/Tutorials'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Dashboard />}></Route>
+        <Route path="/auth" element={<Auth />}></Route>
+        <Route path='/blog' element={<Blog />}></Route>
+        <Route path="/products" element={<Product />}></Route>
+        <Route path="/tutorials" element={<Tutorials />}></Route>
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
