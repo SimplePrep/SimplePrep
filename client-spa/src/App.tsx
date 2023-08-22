9cc4535f0c56b59bcc4195642c71ecffaf9ec84f
Alijon Karimberdiev <coding802@gmail.com>
2023-08-22 17:29:49 -0400

    Created Student Dashboard and Added Contents Section

--- client-spa/src/App.tsx
+++ client-spa/src/App.tsx
@@ -4,11 +4,13 @@ import './App.css';
 import Navbar from './components/navbar/navbar';
 import LandingPage from './pages/LandingPage';
 import { Route, Routes } from 'react-router';
+import DashboardPage from './pages/DashboardPage';
 
 function App() {
   return (
     <Routes>
       <Route path = '/' element={<LandingPage/>}/>
+      <Route path = '/Demo' element={<DashboardPage/>}/>
     </Routes>
   );
 }
