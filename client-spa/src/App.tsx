6dd86d16bd48d4a0fbdcbfd4f5172ad3e0e4efa0
Alijon Karimberdiev <79594266+alijon30@users.noreply.github.com>
2023-08-22 17:39:47 -0400

    Revert "Created Student Dashboard and Added Contents Section"

--- client-spa/src/App.tsx
+++ client-spa/src/App.tsx
@@ -4,13 +4,11 @@ import './App.css';
 import Navbar from './components/navbar/navbar';
 import LandingPage from './pages/LandingPage';
 import { Route, Routes } from 'react-router';
-import DashboardPage from './pages/DashboardPage';
 
 function App() {
   return (
     <Routes>
       <Route path = '/' element={<LandingPage/>}/>
-      <Route path = '/Demo' element={<DashboardPage/>}/>
     </Routes>
   );
 }
