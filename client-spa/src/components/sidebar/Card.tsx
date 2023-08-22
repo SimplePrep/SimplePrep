5b004fa7e39e8a4897bc76248d7c52ef662d8845
Alijon Karimberdiev <coding802@gmail.com>
2023-08-22 18:39:16 -0400

    Added hover effect to the Card Component and removed overflow-hidden as it was not allowing the hover effect to scale up

--- client-spa/src/components/sidebar/Card.tsx
+++ client-spa/src/components/sidebar/Card.tsx
@@ -7,26 +7,26 @@ const Card = () => {
     <div className='h-full w-full'>
         <div className='max-w-[1450px] mx-auto'>
             <p className='p-5 text-4xl font-bold text-center text-[#00df9a]'>Contents</p>
-            <div className='my-24 grid lg:grid-cols-4 gap-5 overflow-hidden'>
-                <div className=' bg-white border-dashed border-cyan-100 rounded-2xl items-center shadow-lg'>
+            <div className='my-24 grid lg:grid-cols-4 gap-5'>
+                <div className=' bg-white border-dashed border-cyan-100 rounded-2xl items-center shadow-lg hover:scale-105 duration-300'>
                     <p className='text-center p-4 text-xl font-bold'>Authentic Practice Tests</p>
                     <hr />
                     <img className='w-full' src={pic1} alt="" />
                     <p className='text-center mx-24 p-3 font-medium rounded-xl bg-green-500'>Get Started</p>
                 </div>
-                <div className=' bg-slate-50 border-dashed border-cyan-100 rounded-2xl items-center shadow-lg'>
+                <div className=' bg-slate-50 border-dashed border-cyan-100 rounded-2xl items-center shadow-lg hover:scale-105 duration-300' >
                     <p className='text-center p-4 text-xl font-bold'>Topic-based Questions</p>
                     <hr />
                     <img className='w-full' src={pic2} alt="" />
                     <p className='text-center mx-24 p-3 font-medium rounded-xl bg-green-500'>Get Started</p>
                 </div>
-                <div className=' bg-slate-50 border-dashed border-cyan-100 rounded-2xl items-center shadow-lg'>
+                <div className=' bg-slate-50 border-dashed border-cyan-100 rounded-2xl items-center shadow-lg hover:scale-105 duration-300'>
                     <p className='text-center p-4 text-xl font-bold'>English & Writing</p>
                     <hr />
                     <img className='w-full' src={pic2} alt="" />
                     <p className='text-center mx-24 p-3 font-medium rounded-xl bg-green-500'>Get Started</p>
                 </div>
-                <div className=' bg-slate-50 border-dashed border-cyan-100 rounded-2xl items-center shadow-lg'>
+                <div className=' bg-slate-50 border-dashed border-cyan-100 rounded-2xl items-center shadow-lg hover:scale-105 duration-300'>
                     <p className='text-center p-4 text-xl font-bold'>Math</p>
                     <hr />
                     <img className='w-full' src={pic4} alt="" />
