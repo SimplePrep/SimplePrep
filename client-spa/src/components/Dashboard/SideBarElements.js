b08e0ac7885ef1cba3b277c06d5694bd8c57f93d
Alijon Karimberdiev <coding802@gmail.com>
2023-08-31 12:57:16 -0400

    Updated maintainability of code

---
+++ client-spa/src/components/Dashboard/SideBarElements.js
@@ -0,0 +1,56 @@
+import { AiOutlineSetting } from "react-icons/ai";
+import { BsDiscord } from "react-icons/bs";
+import { GiProgression } from "react-icons/gi";
+import { IoNotificationsCircleOutline } from "react-icons/io5";
+import { MdOutlineAssignment, MdOutlineContactSupport } from "react-icons/md";
+import { PiSignOutFill } from "react-icons/pi";
+import { TbLayoutDashboard } from "react-icons/tb";
+import { VscBook } from "react-icons/vsc";
+
+export const SideBarLinks = [
+    {
+        title : "Dashboard",
+        path: '/dashboard',
+        icon: <TbLayoutDashboard size={35} color='green' />
+    },
+    {
+        title: "Notificattions",
+        path: '/notifications',
+        icon: <IoNotificationsCircleOutline size={35} color='green' />
+    },
+    {
+        title: "Contents",
+        path: '/contents',
+        icon: <MdOutlineAssignment size={35} color='green' />
+    },
+    {
+        title: "Tutorials",
+        path: '/tutorials',
+        icon: <VscBook size={35} color='green' />
+    },
+    {
+        title: "Our Discord Channel",
+        path: 'https://discord.gg/7c93kyfC',
+        icon: <BsDiscord size={35} color='green' />
+    },
+    {
+        title: "Track your progress",
+        path: '/track',
+        icon: <GiProgression size={35} color='green' />
+    },
+    {
+        title: "Help",
+        path: '/help',
+        icon: <MdOutlineContactSupport size={35} color='green' />
+    },
+    {
+        title: "Settings",
+        path: '/settings',
+        icon: <AiOutlineSetting size={35} color='green' />
+    },
+    {
+        title: "Sign Out",
+        path: '/sign-out',
+        icon: <PiSignOutFill size={35} color='green' />
+    }
+]
--- client-spa/src/pages/DashboardPage.tsx
+++ client-spa/src/pages/DashboardPage.tsx
@@ -4,25 +4,25 @@ import SideBar from '../components/Dashboard/SideBar'
 import Tutorials from '../components/Dashboard/Tutorials'
 import Blogs from '../components/Dashboard/Blogs'
 import Contents from '../components/Dashboard/Contents'
-import { Outlet } from 'react-router-dom'
+import { BrowserRouter, Outlet } from 'react-router-dom'
 
 const DashboardPage = () => {
   return (
-    <div className='w-full h-full bg-green-300'>
-        <div className='w-full h-full rounded-lg p-10 '>
-            <div className='w-full h-full rounded-2xl bg-white'>
-                <div className='flex flex-row gap-10'>
-                    <div className='w-[300px] border-r h-[92.5vh]'>
-                        <SideBar/>
-                        <Outlet/>
-                    </div>
-                    <div className='flex-grow'>
-                        <Contents/>
+        <div className='w-full h-full bg-green-300'>
+            <div className='w-full h-full rounded-lg p-10 '>
+                <div className='w-full h-full rounded-2xl bg-white'>
+                    <div className='flex flex-row gap-10'>
+                        <div className='w-[300px] border-r h-[92.5vh]'>
+                            <SideBar/>
+                            <Outlet/>
+                        </div>
+                        <div className='flex-grow'>
+                            <Contents/>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
   )
 }
 
