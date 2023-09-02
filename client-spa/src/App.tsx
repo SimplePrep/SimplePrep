6962f3136cc21e8b2841323e57c0eca9606d5039
Alijon Karimberdiev <coding802@gmail.com>
2023-09-02 00:06:00 -0400

    Added Test UI and some functionality to it.

--- client-spa/src/App.tsx
+++ client-spa/src/App.tsx
@@ -7,6 +7,8 @@ import DashboardPage from './pages/DashboardPage';
 import LandingPage from './pages/LandingPage';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import APT from './components/Dashboard/APT';
+import Test from './components/Dashboard/Test';
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
         <Route path = 'Demo/*' element={<DashboardPage/>}/>
         <Route path = "sign-in" element = {<SignIn/>}/>
         <Route path = "sign-up" element = {<SignUp/>}/>
+        <Route path = "practice-tests" element = {<APT/>}/>
+        <Route path = "test" element = {<Test/>}/>
       </Routes>
   );
 }
