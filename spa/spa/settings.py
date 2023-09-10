4f411e4935cc194bed33c3622196380aab5d1408
Alijon Karimberdiev <coding802@gmail.com>
2023-09-10 19:15:06 -0400

    Set up Docker container for backend spa

--- spa/spa/settings.py
+++ spa/spa/settings.py
@@ -74,9 +74,12 @@ WSGI_APPLICATION = "spa.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'localhost',
+        'NAME': 'tests_db',
+        'USER': 'devuser',
+        'PASSWORD' : 'changeme',
     }
 }
 
