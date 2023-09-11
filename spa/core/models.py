b5fae4d38dffd2cea9c5f723877fd6429dd8347e
Alijon Karimberdiev <coding802@gmail.com>
2023-09-11 01:23:40 -0400

    Created model for BlogPost

--- spa/core/models.py
+++ spa/core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.utils.timesince import timesince
+import datetime
 # Create your models here.
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    pub_date = models.DateTimeField("Date published")
+    reading_time = models.PositiveIntegerField(help_text="Estimated reading time (minutes)")
+    
+    content = models.TextField()
+
+    def __str__(self) -> str:
+        current_date = datetime.datetime.now()
+        if self.pub_date.year == current_date.year:
+            pub_date = self.pub_date.strftime("%b %d")
+        else:
+            pub_date = self.pub_date.strftime("%b %d, %Y")
+        return f"{self.title} || {pub_date}"
