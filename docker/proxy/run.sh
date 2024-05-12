32ee9fb74101f287fffa5eaa8df2965f33afa86f
Alijon Karimberdiev <coding802@gmail.com>
2024-05-12 03:41:03 -0400

    updated

--- docker/proxy/run.sh
+++ docker/proxy/run.sh
@@ -3,19 +3,21 @@
 set -e
 
 echo "Checking for dhparams.pem"
-if [ ! -f "/vol/proxy/ssl-dhparams.pem"]; then
+# Added a space before the closing bracket
+if [ ! -f "/vol/proxy/ssl-dhparams.pem" ]; then
     echo "dhparams.pem does not exist - creating it"
     openssl dhparam -out /vol/proxy/ssl-dhparams.pem 2048
 fi 
 
-#Avoid replacting these with envsubst
+# Avoid replacing these with envsubst
 export host=\$host 
 export request_uri=\$request_uri
 
 echo "Checking for fullchain.pem"
-if [ ! -f "/etc/letsencrypt/live/${DOMAIN}/fullchain.pem"]; then
+# Added a space before the closing bracket
+if [ ! -f "/etc/letsencrypt/live/${DOMAIN}/fullchain.pem" ]; then
     echo "No SSL cert, enabling HTTP only..."
-    envsubst  < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
+    envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
 else 
     echo "SSL cert exists, enabling HTTPS..."
     envsubst < /etc/nginx/default-ssl.conf.tpl > /etc/nginx/conf.d/default.conf
