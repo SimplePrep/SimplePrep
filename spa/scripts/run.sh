c5ab04fc7e46a05729cf30ae514ae8a1362df77b
Alijon Karimberdiev <coding802@gmail.com>
2024-05-13 19:16:38 -0400

    update -5

--- spa/scripts/run.sh
+++ spa/scripts/run.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+echo "Debug Info: Starting script"
 echo "Current working directory: $(pwd)"
 echo "Listing contents of the current directory:"
 ls -l
@@ -9,6 +10,7 @@ ls -l
 # Navigate to the root directory where manage.py is located
 cd /spa
 
+echo "Debug Info: After cd to /spa"
 echo "Current working directory after cd: $(pwd)"
 echo "Listing contents of /spa:"
 ls -l /spa
@@ -17,10 +19,13 @@ echo "Listing contents of /spa/spa:"
 ls -l /spa/spa
 
 echo "Running collectstatic..."
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput || { echo 'Collectstatic failed' ; exit 1; }
 
 echo "Running migrations..."
-python manage.py migrate
+python manage.py migrate || { echo 'Migration failed' ; exit 1; }
+
+echo "Running simple Python import test..."
+python -c "import spa; print(spa)" || { echo 'Python import test failed' ; exit 1; }
 
 echo "Starting uWSGI..."
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module spa.wsgi || { echo 'uWSGI failed' ; exit 1; }
