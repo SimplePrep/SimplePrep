81c08ebe2ea0d5c099aa8fd62d0c5c6cdb0cff96
Alijon Karimberdiev <coding802@gmail.com>
2024-05-13 19:03:10 -0400

    update-4

--- spa/scripts/run.sh
+++ spa/scripts/run.sh
@@ -2,11 +2,22 @@
 
 set -e
 
-# Navigate to the root directory where manage.py is located
-cd ../../spa
+echo "Current working directory: $(pwd)"
+echo "Listing contents of the current directory:"
+ls -l
 
+# Navigate to the root directory where manage.py is located
+cd /spa
+
+echo "Current working directory after cd: $(pwd)"
+echo "Listing contents of /spa:"
+ls -l /spa
+
+echo "Running collectstatic..."
 python manage.py collectstatic --noinput
+
+echo "Running migrations..."
 python manage.py migrate
 
+echo "Starting uWSGI..."
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module spa.wsgi
-
