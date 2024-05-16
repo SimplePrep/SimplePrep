e6f226be8e4dd0e5806e5ad1cf7647d29597844d
Alijon Karimberdiev <coding802@gmail.com>
2024-05-15 21:29:44 -0400

    small update tp app.tsx

--- client-spa/src/App.tsx
+++ client-spa/src/App.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import './App.css';
-import { Route, Routes } from 'react-router';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import AuthProvider from './components/utils/AuthProvider';
+import Layout, { LandingPage } from './pages/LandingPage';
 import DashboardPage from './pages/DashboardPage';
-import Layout, {LandingPage} from './pages/LandingPage';
 import Login from './pages/Authentication/Login';
 import SignUp from './pages/Authentication/SignUp';
 import BlogPostDetails from './components/Landing/features/BlogPostDetails';
@@ -12,11 +12,10 @@ import ResetPassword from './pages/Authentication/ResetPassword';
 import Contents from './components/Dashboard/Contents';
 import Tutorials from './components/Dashboard/Tutorials';
 import TutorialPage from './components/Dashboard/TutorialPage';
-import Section, { sections } from './components/Dashboard/Section';
-import { Navigate , BrowserRouter as Router} from 'react-router-dom';
+import Section from './components/Dashboard/Section';
 import Analytics from './components/Dashboard/Analytics';
-import AuthProvider from './components/utils/AuthProvider';
-import { Provider } from 'react-redux';
+import './App.css';
+
 
 
 const App = () => {
@@ -25,33 +24,34 @@ const App = () => {
   const toggleDarkMode = () => setIsDarkMode(!isDarkMode);
 
   return (
-        <AuthProvider>
-          <Routes>
-              <Route path="/" element={<Layout/>}>
-                <Route index element={<LandingPage/>}/>
-                <Route path="blogs" element={<Blogs/>} />
-                <Route path="blogs/:id" element={<BlogPostDetails/>}/>
-              </Route>
-                <Route path="demo" element={<DashboardPage toggleDarkMode={toggleDarkMode} isDarkMode={isDarkMode}/>}>
-                  <Route index element={<Contents/>}/>
-                  <Route path="dashboard" element={<Contents/>}/>
-                  <Route path="tutorials" element={<Tutorials/>}/>
-                  <Route path="tutorials/:tutorialId" element={<TutorialPage  isDarkMode={isDarkMode}/>}>
-                    <Route index element={<Navigate to={`command-of-evidence`} replace />} />
-                    <Route path=":sectionId" element={<Section isDarkMode={isDarkMode}/>}/>
-                    <Route path=":sectionId/:subsectionId" element={<Section isDarkMode={isDarkMode}/>}/>
-                  </Route>
-                  <Route path='analytics' element={<Analytics isDarkMode={isDarkMode} />}/>
-                </Route>
-              {/* Authentication Routes */}
-              <Route path="/login" element={<Login/>}/>
-              <Route path="/signup" element={<SignUp/>}/>
-              <Route path="/reset-password" element={<ResetPassword/>}/>
-
-              <Route path="practice-tests/:testId/sections/:sectionId/:sectionName" element={<TestPageUI/>}/>
-              <Route path="test" element={<TestPageUI/>}/>
-          </Routes>
-        </AuthProvider>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<LandingPage />} />
+            <Route path="blogs" element={<Blogs />} />
+            <Route path="blogs/:id" element={<BlogPostDetails />} />
+          </Route>
+          <Route path="demo" element={<DashboardPage toggleDarkMode={toggleDarkMode} isDarkMode={isDarkMode} />}>
+            <Route index element={<Contents />} />
+            <Route path="dashboard" element={<Contents />} />
+            <Route path="tutorials" element={<Tutorials />} />
+            <Route path="tutorials/:tutorialId" element={<TutorialPage isDarkMode={isDarkMode} />}>
+              <Route index element={<Navigate to={`command-of-evidence`} replace />} />
+              <Route path=":sectionId" element={<Section isDarkMode={isDarkMode} />} />
+              <Route path=":sectionId/:subsectionId" element={<Section isDarkMode={isDarkMode} />} />
+            </Route>
+            <Route path='analytics' element={<Analytics isDarkMode={isDarkMode} />} />
+          </Route>
+          {/* Authentication Routes */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="practice-tests/:testId/sections/:sectionId/:sectionName" element={<TestPageUI />} />
+          <Route path="test" element={<TestPageUI />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
     
   );
 }
