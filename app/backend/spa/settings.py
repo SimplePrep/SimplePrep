a206419d34c67591a579974285ae0a19456ddd14
Alijon Karimberdiev <coding802@gmail.com>
2024-05-17 08:30:51 -0400

    Update settings.py

--- app/backend/spa/settings.py
+++ app/backend/spa/settings.py
@@ -150,9 +150,10 @@ REST_FRAMEWORK = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_URL = '/django_static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'django_static')
 MEDIA_URL = '/media/'
-STATIC_URL = '/django_static/' 
-STATIC_ROOT = BASE_DIR / 'django_static'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
