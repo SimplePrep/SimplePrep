709bb255362aaaae4e9a5a971b5d59986e0d78e6
Alijon Karimberdiev <coding802@gmail.com>
2024-05-18 01:20:16 -0400

    Update urls.py

--- app/backend/spa/urls.py
+++ app/backend/spa/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+import two_factor
 from two_factor.admin import AdminSiteOTPRequired
 
 admin.site.__class__ = AdminSiteOTPRequired
@@ -28,6 +29,7 @@ urlpatterns = [
     # path('api/token/', TokenObtainPairView.as_view()),
     # path('api/token/refresh/', TokenRefreshView.as_view()),
     # path('api/token/verify/', TokenVerifyView.as_view()),
+    path('', include('two_factor.urls', 'two_factor')),
     path("my-django-admin/", admin.site.urls),
     path("auth/user/", include('user.urls')),
     path("api/core/", include('core.urls')),
