0601d5de772a773c0963a52768c47b76ee26aba9
Alijon Karimberdiev <coding802@gmail.com>
2024-05-20 15:51:04 -0400

    Update axiosServices.tsx

--- app/frontend/src/components/utils/axios/axiosServices.tsx
+++ app/frontend/src/components/utils/axios/axiosServices.tsx
@@ -3,7 +3,7 @@ import axiosInstance from './axiosInterceptor';
 // Example function to get tests
 export const getTests = async () => {
   try {
-    const response = await axiosInstance.get('/tests');
+    const response = await axiosInstance.get('api/core/tests');
     return response.data;
   } catch (error) {
     console.error('Error fetching tests:', error);
