e4ffd3a48857d7a8113868f8b04e813b5eba6fbe
Alijon Karimberdiev <coding802@gmail.com>
2024-05-21 22:36:58 -0400

    Update wsgi-entrypoint.sh

--- app/docker/backend/wsgi-entrypoint.sh
+++ app/docker/backend/wsgi-entrypoint.sh
@@ -6,7 +6,7 @@ do
 done
 
 # Generate migrations
-until ./manage.py makemigrations --noinput
+until ./manage.py makemigrations core
 do
     echo "Waiting for makemigrations to be ready..."
     sleep 2
