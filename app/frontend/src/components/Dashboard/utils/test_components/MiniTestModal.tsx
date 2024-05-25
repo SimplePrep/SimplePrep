91dc3a5fa8da30164e17aee20ee1bae752c09d30
Alijon Karimberdiev <coding802@gmail.com>
2024-05-24 21:47:15 -0400

    Update MiniTestModal.tsx

--- app/frontend/src/components/Dashboard/utils/test_components/MiniTestModal.tsx
+++ app/frontend/src/components/Dashboard/utils/test_components/MiniTestModal.tsx
@@ -1,7 +1,8 @@
 // MiniTestModal.tsx
 import React, { useState, useEffect } from 'react';
 import { BsFillArrowLeftCircleFill, BsFillArrowRightCircleFill, BsMoon } from 'react-icons/bs';
-import {Question, UserAnswer} from '../../types';
+import { Question, UserAnswer } from '../../types';
+
 interface MiniTestProps {
     isOpen: boolean;
     onClose: () => void;
@@ -10,12 +11,14 @@ interface MiniTestProps {
 }
 
 const MiniTestModal: React.FC<MiniTestProps> = ({ isOpen, onClose, questions, userAnswers }) => {
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const [selectedChoice, setSelectedChoice] = useState<string | null>(null);
     const [fadeIn, setFadeIn] = useState(false);
-
     const [isDarkMode, setIsDarkMode] = useState(false);
-    const darkModeClass = isDarkMode ? 'dark' : 'bg-white text-black';
+
+    const darkModeClass = isDarkMode ? 'dark bg-gray-800 text-white' : 'bg-white text-black';
     const animationClass = fadeIn ? 'animate-fadeIn' : 'opacity-0';
+
     const toggleDarkMode = () => {
         setIsDarkMode(!isDarkMode);
     };
@@ -27,14 +30,42 @@ const MiniTestModal: React.FC<MiniTestProps> = ({ isOpen, onClose, questions, us
         return () => clearTimeout(timer);
     }, []);
 
+    useEffect(() => {
+        if (questions.length > 0) {
+            const currentQuestion = questions[currentQuestionIndex];
+            const userAnswer = userAnswers.find(answer => answer.questionId === currentQuestion.id);
+            setSelectedChoice(userAnswer ? userAnswer.selectedChoice : null);
+        }
+    }, [currentQuestionIndex, questions, userAnswers]);
+
     if (!isOpen) return null;
+
+    const currentQuestion = questions[currentQuestionIndex];
+    const answerChoices = [
+        { label: 'A', content: currentQuestion.option_A },
+        { label: 'B', content: currentQuestion.option_B },
+        { label: 'C', content: currentQuestion.option_C },
+        { label: 'D', content: currentQuestion.option_D },
+    ];
+
+    const handlePreviousQuestion = () => {
+        if (currentQuestionIndex > 0) {
+            setCurrentQuestionIndex(currentQuestionIndex - 1);
+        }
+    };
+
+    const handleNextQuestion = () => {
+        if (currentQuestionIndex < questions.length - 1) {
+            setCurrentQuestionIndex(currentQuestionIndex + 1);
+        }
+    };
 
     return (
         <div className={`fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-50 ${animationClass}`}>
-            <div className={`relative top-20 max-w-[1400px] mx-auto p-5 border  shadow-lg rounded-2xl ${darkModeClass}`}>
+            <div className={`relative top-20 max-w-[1400px] mx-auto p-5 border shadow-lg rounded-2xl ${darkModeClass}`}>
                 <div className='flex p-5 justify-between items-center'>
                     <div className='mx-5 flex gap-10 items-center'>
-                        <p className='text-bold font-ubuntu text-2xl'>Quiz</p>
+                        <p className='font-bold text-2xl'>Test Preview</p>
                         <button onClick={toggleDarkMode} className="text-lg p-3 border-2 rounded-2xl hover:bg-[#00df9a] hover:border-blue-500">
                             <BsMoon />
                         </button>
@@ -47,42 +78,45 @@ const MiniTestModal: React.FC<MiniTestProps> = ({ isOpen, onClose, questions, us
                 <div className='flex flex-grow'>
                     <div className='w-[50%] border-r-2'>
                         <div className="p-14">
-                            <p className='font-medium text-lg'>
-                                Studies have shown that people’s perception of pain can be diminished and their healing processes accelerated if those individuals believe that a medical treatment will be helpful. In an experiment testing the efficacy of an experimental analgesic lotion for treating minor scrapes and burns, researchers found that the lotion did not appear to significantly affect participants’ pain levels or healing times. However, a similar experiment found that the same lotion led to significant reductions in the pain level of study participants and resulted in faster healing. The difference in the outcomes of the two experiments was likely due to the fact that _______
-                            </p>
+                            <p className='font-medium text-lg'>{currentQuestion.context}</p>
                         </div>
                     </div>
                     <div className='w-[50%]'>
                         <div className='p-14'>
                             <div className='flex gap-2 items-center'>
-                                <p className='font-bold text-lg'>Question 1</p>
+                                <p className='font-bold text-lg'>Question {currentQuestionIndex + 1}</p>
                             </div>
-                            <p className='font-medium text-lg mt-3'>Which choice most logically completes the text?</p>
+                            <p className='font-medium text-lg mt-3'>{currentQuestion.query}</p>
                             <div className='flex flex-col mt-7 gap-2'>
-                                {questions.map((question, index) => (
+                                {answerChoices.map((choice, index) => (
                                     <button
                                         key={index}
                                         className={`py-2 px-4 border-2 rounded-lg font-semibold text-lg w-full text-left 
-                                                    ${selectedChoice === question.correct_answer ? 'border-[#00df9a]' : 'hover:border-blue-500'}`}
-                                        onClick={() => setSelectedChoice(question.correct_answer)}
+                                                    ${selectedChoice === choice.label ? 'border-blue-500' : 'hover:border-blue-500'}
+                                                    ${currentQuestion.correct_answer === choice.label ? 'bg-green-200' : ''}
+                                                    ${selectedChoice === choice.label && currentQuestion.correct_answer !== choice.label ? 'bg-red-200' : ''}`}
+                                        onClick={() => setSelectedChoice(choice.label)}
                                     >
-                                        {`(${question.correct_answer})`} {question.query}
+                                        {`(${choice.label})`} {choice.content}
                                     </button>
                                 ))}
                             </div>
+                            <p className='mt-5 font-medium text-md'>Explanation: {currentQuestion.explanation}</p>
                         </div>
                     </div>
                 </div>
                 <div className="flex justify-between p-5">
                     <button
-                        onClick={() => { /* Implement previous question logic */ }}
+                        onClick={handlePreviousQuestion}
                         className="py-2 px-6 border-2 rounded-xl hover:bg-[#00df9a] hover:border-blue-500 hover:text-white font-semibold text-lg"
+                        disabled={currentQuestionIndex === 0}
                     >
                         <BsFillArrowLeftCircleFill className="inline mr-2" /> Previous
                     </button>
                     <button
-                        onClick={() => { /* Implement next question logic */ }}
+                        onClick={handleNextQuestion}
                         className="py-2 px-6 border-2 rounded-xl hover:bg-[#00df9a] hover:border-blue-500 hover:text-white font-semibold text-lg"
+                        disabled={currentQuestionIndex === questions.length - 1}
                     >
                         Next <BsFillArrowRightCircleFill className="inline ml-2" />
                     </button>
