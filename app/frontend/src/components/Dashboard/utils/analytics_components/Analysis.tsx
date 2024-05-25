c0b34ece85754fc938dfcf6d8f81c7e0ac728cca
Alijon Karimberdiev <coding802@gmail.com>
2024-05-25 11:25:13 -0400

    Update Analysis.tsx

--- app/frontend/src/components/Dashboard/utils/analytics_components/Analysis.tsx
+++ app/frontend/src/components/Dashboard/utils/analytics_components/Analysis.tsx
@@ -27,6 +27,10 @@ const Analysis: React.FC<AnalysisProps> = ({ data, onClose }) => {
     return () => clearTimeout(timer);
   }, []);
 
+  const roundToTwoDecimals = (num: number) => {
+    return Math.round(num * 100) / 100;
+  };
+
   return (
     <div className={`fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-50 ${animationClass}`}>
       <div className={`relative top-20 mb-8 max-w-[1400px] mx-auto p-5 border shadow-lg rounded-2xl ${darkModeClass}`}>
@@ -50,7 +54,7 @@ const Analysis: React.FC<AnalysisProps> = ({ data, onClose }) => {
                 {Object.entries(moduleData.sections).map(([sectionName, sectionData]) => (
                   <div key={sectionName} className='flex items-center'>
                     <div className='py-5 relative flex flex-col gap-5 justify-center items-center w-full'>
-                      <AnalyticsChart value={(sectionData.correct_answers / sectionData.total_questions) * 100} maxValue={100} />
+                      <AnalyticsChart value={roundToTwoDecimals((sectionData.correct_answers / sectionData.total_questions) * 100)} maxValue={100} />
                       <p className=''><span className='font-bold'>{sectionData.correct_answers}</span> Out of {sectionData.total_questions} Correct</p>
                     </div>
                     <p className='text-lg font-medium'>{sectionName}</p>
@@ -59,26 +63,26 @@ const Analysis: React.FC<AnalysisProps> = ({ data, onClose }) => {
               </div>
             </div>
           ))}
-            <div className='w-1/3 flex flex-col gap-10 p-2 items-center rounded-lg'>
-                <div className='p-2 justify-center items-center'>
-                    <h2 className='text-3xl font-medium'>Overall</h2>
-                    <div className='py-5 flex flex-col gap-5 items-center w-full'>
-                    <AnalyticsChart value={(data.correct_answers / data.total_questions) * 100} maxValue={100} />
-                    <p><span className='font-bold'>{data.correct_answers}</span> Out of {data.total_questions} Correct</p>
-                    </div>
-                    <p className='text-lg text-gray-600'>
-                    Quick Note: Topics are sorted by importance based on your test performance.
-                    </p>
-                    <div className='pt-5 w-full'>
-                    <h3 className='font-medium text-2xl'>Topics to Review:</h3>
-                    <ul className='list-disc list-inside my-4'>
-                        {data.suggestions.map((suggestion, index) => (
-                        <li key={index}>{suggestion}</li>
-                        ))}
-                    </ul>
-                    </div>
-                </div>
+          <div className='w-1/3 flex flex-col gap-10 p-2 items-center rounded-lg'>
+          <div className='p-2 justify-center items-center'>
+            <h2 className='text-3xl font-medium'>Overall</h2>
+            <div className='py-5 flex flex-col gap-5 items-center w-full'>
+              <AnalyticsChart value={roundToTwoDecimals((data.correct_answers / data.total_questions) * 100)} maxValue={100} />
+              <p><span className='font-bold'>{data.correct_answers}</span> Out of {data.total_questions} Correct</p>
             </div>
+            <p className='text-lg text-gray-600'>
+              Quick Note: Topics are sorted by importance based on your test performance.
+            </p>
+            <div className='pt-5 w-full'>
+              <h3 className='font-medium text-2xl'>Topics to Review:</h3>
+              <ul className='list-disc list-inside my-4'>
+                {data.suggestions.map((suggestion, index) => (
+                  <li key={index}>{suggestion}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
         </div>
       </div>
     </div>
