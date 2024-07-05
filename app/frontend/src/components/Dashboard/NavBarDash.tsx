4c61fbc7701962374927d2ffa6ea5e93af8194db
Alijon Karimberdiev <coding802@gmail.com>
2024-07-05 09:14:25 -0500

    added toggle to navbar

--- app/frontend/src/components/Dashboard/NavBarDash.tsx
+++ app/frontend/src/components/Dashboard/NavBarDash.tsx
@@ -33,47 +33,49 @@ const SideBar: React.FC<SideBarProps> = ({ toggleDarkMode, isDarkMode }): React.
   };
 
   return (
-    <div className={`fixed top-0 left-0 w-full z-10`}>
-      <div className={`flex justify-end p-2`}>
-        <button onClick={toggleVisibility} className='text-xl p-2 border-2 rounded-full hover:bg-white hover:border-blue-700'>
-          {isVisible ? <BsChevronUp /> : <BsChevronDown />}
-        </button>
-      </div>
-      <motion.div
-        initial={{ height: 0, opacity: 0 }}
-        animate={{ height: isVisible ? 'auto' : 0, opacity: isVisible ? 1 : 0 }}
-        transition={{ type: 'spring', stiffness: 300, damping: 24 }}
-        className={`overflow-hidden shadow-xl max-w-[1300px] mx-auto shadow-blue-300 bg-white rounded-3xl ${darkModeClass} border-2 border-white`}
-      >
-        <div className='flex gap-3 items-center justify-center p-5'>
-          <img className='w-[200px]' src={Logo} alt="" />
-          {SideBarLinks.map((link) => (
-            <NavLink
-              to={link.path}
-              key={link.title}
-              className={({ isActive }) =>
-                isActive
-                  ? "text-xl text-black font-medium leading-tight border-2 border-blue-700 hover:border-gray-200 bg-white p-4 mx-4 mt-2 rounded-3xl cursor-pointer"
-                  : "text-xl font-medium leading-tight hover:bg-white border-2 hover:border-blue-700 hover:text-black hover:rounded-3xl p-4 mx-4 mt-2 rounded-3xl cursor-pointer"
-              }
-            >
-              <div className='flex flex-row items-center gap-3'>
-                {link.icon}
-                {link.title}
-              </div>
-            </NavLink>
-          ))}
-          <button onClick={toggleDarkMode} className='text-xl p-3 border-2 rounded-2xl hover:bg-white hover:border-blue-700'>
-            <BsMoon />
-          </button>
-          <button onClick={handleSignOut} className='text-xl font-medium p-3 border-2 rounded-2xl hover:bg-white hover:border-blue-500 hover:text-black'>
-            Logout
-          </button>
-          <div className='flex items-center justify-center h-12 w-12 rounded-full bg-slate-300 text-xl font-medium text-gray-700 cursor-pointer'>
-            {userInitial}
+    <div className={`fixed top-0 left-0 w-full z-40`}>
+      <div className=' flex flex-row'>
+        <motion.div
+          initial={{ height: 0, opacity: 0 }}
+          animate={{ height: isVisible ? 'auto' : 0, opacity: isVisible ? 1 : 0 }}
+          transition={{ type: 'spring', stiffness: 300, damping: 24 }}
+          className={`overflow-hidden shadow-xl max-w-[1300px] mx-auto shadow-blue-300 bg-white rounded-3xl ${darkModeClass} border-4 border-white`}
+        >
+          <div className='flex gap-3 items-center justify-center p-5'>
+            <img className='w-[200px]' src={Logo} alt="" />
+            {SideBarLinks.map((link) => (
+              <NavLink
+                to={link.path}
+                key={link.title}
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-xl text-black font-medium leading-tight border-2 border-blue-700 hover:border-gray-200 bg-white p-4 mx-4 mt-2 rounded-3xl cursor-pointer"
+                    : "text-xl font-medium leading-tight hover:bg-white border-2 hover:border-blue-700 hover:text-black hover:rounded-3xl p-4 mx-4 mt-2 rounded-3xl cursor-pointer"
+                }
+              >
+                <div className='flex flex-row items-center gap-3'>
+                  {link.icon}
+                  {link.title}
+                </div>
+              </NavLink>
+            ))}
+            <button onClick={toggleDarkMode} className='text-xl p-3 border-2 rounded-2xl hover:bg-white hover:border-blue-700'>
+              <BsMoon />
+            </button>
+            <button onClick={handleSignOut} className='text-xl font-medium p-3 border-2 rounded-2xl hover:bg-white hover:border-blue-500 hover:text-black'>
+              Logout
+            </button>
+            <div className='flex items-center justify-center h-12 w-12 rounded-full bg-slate-300 text-xl font-medium text-gray-700 cursor-pointer'>
+              {userInitial}
+            </div>
           </div>
+        </motion.div>
+        <div className='flex items-center p-2'>
+            <button onClick={toggleVisibility} className='text-xl p-2 border-[3px] rounded-full hover:bg-white hover:border-blue-700'>
+              {isVisible ? <BsChevronUp /> : <BsChevronDown />}
+            </button>
         </div>
-      </motion.div>
+      </div>
     </div>
   );
 };
