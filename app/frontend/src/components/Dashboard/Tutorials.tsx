921e742a11552957890f0f3d16f69fe3df5d5672
Alijon Karimberdiev <coding802@gmail.com>
2024-07-05 04:49:10 -0500

    displaying chapters on TutorialCard

--- app/frontend/src/components/Dashboard/Tutorials.tsx
+++ app/frontend/src/components/Dashboard/Tutorials.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useResolvedPath, NavLink } from 'react-router-dom';
-import { Tutorial } from '../utils/types';
+import { Tutorial, Chapter} from '../utils/types';
 import tutorial1Image from '../assets/tutorials/tutorial1.jpg';
 import tutorial2Image from '../assets/tutorials/tutorial2.jpg';
-import { getTutorials } from '../utils/axios/axiosServices';
+import { getChapters, getTutorials } from '../utils/axios/axiosServices';
+
 
 const basePath = "/demo/tutorials";
 
@@ -12,6 +13,19 @@ interface TutorialCardProps {
 }
 
 const TutorialCard: React.FC<TutorialCardProps> = ({ tutorial }) => {
+  const [chapters, setChapters] = useState<Chapter[]>([]);
+  
+  useEffect(()=> {
+    const fetchChapters = async ()=> {
+      try{
+        const data = await getChapters(tutorial.id);
+        setChapters(data);
+      } catch(error){
+        console.error('Error fetching chapters in TutorialCard:', error);
+      }
+    };
+    fetchChapters();
+  }, []);
   const { pathname } = useResolvedPath(`${basePath}/${tutorial.id}`);
 
   const tutorialImages: { [key: number]: string } = {
@@ -27,6 +41,9 @@ const TutorialCard: React.FC<TutorialCardProps> = ({ tutorial }) => {
         <img className="h-56 object-cover shadow-2xl shadow-teal-100" src={tutorialImage} alt={`${tutorial.title} icon`} />
         <div className="px-6 py-4">
           <p className="font-bold text-xl mb-2">{tutorial.title}</p>
+          <ul className='text-gray-700 text-base'>
+              {chapters.map((chapter, index) => <li key={index}>{chapter.title}</li>)}
+          </ul>
         </div>
         <div className='flex justify-center pb-4'>
           <NavLink to={pathname} className="mx-auto py-2 px-4 bg-blue-500 text-white text-lg rounded hover:bg-blue-600 transition-colors duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-300 focus:ring-opacity-50">
