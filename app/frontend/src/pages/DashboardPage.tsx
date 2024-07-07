161495092ef83dd6775576efff53679a98831d08
Alijon Karimberdiev <coding802@gmail.com>
2024-07-07 08:21:35 -0500

    Adding bookloader

--- app/frontend/src/pages/DashboardPage.tsx
+++ app/frontend/src/pages/DashboardPage.tsx
@@ -4,6 +4,7 @@ import NavBarDash from '../components/Dashboard/NavBarDash';
 import { useSelector, useDispatch } from 'react-redux';
 import { AppDispatch, RootState } from '../components/store';
 import { checkAuthenticated, loadUser } from '../components/auth_utils/actions/authActions';
+import BookLoader from '../components/Dashboard/utils/bookLoader';
 
 interface DashboardPageProps {
   toggleDarkMode: () => void;
@@ -35,12 +36,19 @@ const DashboardPage: React.FC<DashboardPageProps> = ({
     }
   }, [loading, isAuthenticated, initialLoad, navigate]);
 
-  if (initialLoad || loading) {
-    return <div>Loading...</div>;  // Or any other loading indicator
-  }
-
   return (
-    <div className={`w-full h-full ${darkModeClass} font-opensans`}>
+    <div className={`w-full h-full ${darkModeClass} font-opensans relative`}>
+      {(initialLoad || loading) && (
+        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-md z-50">
+          <BookLoader
+            background="linear-gradient(135deg, #6066FA, #4645F6)"
+            desktopSize="100px"
+            mobileSize="80px"
+            textColor="#4645F6"
+            duration="5s"
+          />
+        </div>
+      )}
       <NavBarDash toggleDarkMode={toggleDarkMode} isDarkMode={isDarkMode} />
       <Outlet />
     </div>
