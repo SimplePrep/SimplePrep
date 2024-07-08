f387a38ce6d0b5ea27b3300663611b788286472f
Alijon Karimberdiev <coding802@gmail.com>
2024-07-08 13:10:17 -0500

    compressed images for faster load

--- app/frontend/src/components/Landing/features/BlogPostDetails.tsx
+++ app/frontend/src/components/Landing/features/BlogPostDetails.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import { useParams } from 'react-router-dom'
 import { BlogPost } from '../../models/blog';
-import img1 from '../../assets/example2.png'
+import img1 from '../../assets/example2.webp'
 
 interface Props { 
   text: string | null;
