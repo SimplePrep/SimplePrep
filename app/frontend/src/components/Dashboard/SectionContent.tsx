f68e1406ac91c798614622b8e2fa50b8204016fb
Alijon Karimberdiev <coding802@gmail.com>
2024-07-17 03:26:40 -0500

    updated TutorialPage

--- app/frontend/src/components/Dashboard/SectionContent.tsx
+++ app/frontend/src/components/Dashboard/SectionContent.tsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { BsFillArrowLeftCircleFill, BsFillArrowRightCircleFill } from 'react-icons/bs';
-import { getSection, getSections } from "../auth_utils/axios/axiosServices";
-import { Section } from "../auth_utils/types";
 import { motion, useScroll, useSpring, AnimatePresence } from "framer-motion";
+import { Section } from "../auth_utils/types";
+import { getSection, getSections } from "../auth_utils/axios/axiosServices";
 
 interface ParagraphProps {
   text: string | null;
@@ -12,7 +12,7 @@ interface ParagraphProps {
 
 const Paragraph: React.FC<ParagraphProps> = ({ text, isHighlighted = false }) => {
   return (
-    <p className={`text-lg leading-relaxed max-w-prose mx-auto ${isHighlighted ? "bg-slate-200 rounded-lg p-4 text-black" : ""} mb-6`}>
+    <p className={`text-xl leading-relaxed ${isHighlighted ? "bg-slate-200 rounded-lg p-4 text-black" : ""} mb-6`}>
       {text}
     </p>
   );
@@ -80,7 +80,7 @@ const SectionContent: React.FC<{ isDarkMode: boolean }> = ({ isDarkMode }) => {
     }
   };
 
-  const modeClass = isDarkMode ? 'bg-[#121212] text-white' : 'bg-white text-gray-800';
+  const modeClass = isDarkMode ? 'text-white transition-colors duration-300' : 'text-[#001a72] transition-colors duration-300';
   const contentToRender = section?.content || '';
   const titleToRender = section?.title || '';
   const descriptionToRender = section?.description || '';
@@ -89,7 +89,7 @@ const SectionContent: React.FC<{ isDarkMode: boolean }> = ({ isDarkMode }) => {
   if (error) return <div className="text-center p-4 text-red-500">{error}</div>;
 
   return (
-    <div className="pt-20">
+    <div className="">
       <AnimatePresence>
         <motion.div
           key={sectionSlug}
@@ -97,24 +97,24 @@ const SectionContent: React.FC<{ isDarkMode: boolean }> = ({ isDarkMode }) => {
           animate={{ y: 0, opacity: 1 }}
           exit={{ y: -10, opacity: 0 }}
           transition={{ duration: 0.2 }}
-          className={`min-h-screen rounded-2xl ${modeClass} py-24`}
+          className={`min-h-screen ${modeClass} py-10`}
         >
           <motion.div className="fixed top-0 left-0 right-0 h-1 bg-blue-500 origin-left z-50" style={{ scaleX }} />
           <div className='flex flex-col gap-6 justify-center items-center'>
             <h1 className='text-center text-4xl font-bold mb-4'>{titleToRender}</h1>
             <p className='text-center text-lg mb-6'>{descriptionToRender}</p>
           </div>
-          <div className='py-5 rounded-lg shadow-md'>
+          <div className=''>
             {contentToRender.split('\n').map((paragraph, index) => {
               const isHighlighted = paragraph.includes("**");
               return <Paragraph key={index} text={paragraph.replace(/\*\*/g, "")} isHighlighted={isHighlighted} />;
             })}
           </div>
-          <div className='p-5 flex justify-between items-center mt-6'>
+          <div className='flex justify-between items-center mt-6'>
             <motion.button
               onClick={handlePreviousClick}
               disabled={currentSectionIndex === 0}
-              className="py-2 px-6 border-2 rounded-xl hover:bg-blue-500 hover:text-white font-semibold text-lg disabled:opacity-50"
+              className={`py-2 px-6 border-2 rounded-xl hover:bg-blue-500 hover:text-white font-semibold text-lg disabled:opacity-50 ${isDarkMode ? 'hover:bg-white hover:text-[#001a72]' :  'hover:bg-[#001a72] hover:text-white'}`}
               whileHover={{ scale: 1.1 }}
               whileTap={{ scale: 0.9 }}
             >
@@ -123,7 +123,7 @@ const SectionContent: React.FC<{ isDarkMode: boolean }> = ({ isDarkMode }) => {
             <motion.button
               onClick={handleNextClick}
               disabled={currentSectionIndex === sections.length - 1}
-              className="py-2 px-6 border-2 rounded-xl hover:bg-blue-500 hover:text-white font-semibold text-lg disabled:opacity-50"
+              className={`py-2 px-6 border-2 rounded-xl hover:bg-blue-500 hover:text-white font-semibold text-lg disabled:opacity-50 ${isDarkMode ? 'hover:bg-white hover:text-[#001a72]' :  'hover:bg-[#001a72] hover:text-white'}`}
               whileHover={{ scale: 1.1 }}
               whileTap={{ scale: 0.9 }}
             >
