05aa2a8d869c2bb65add6503565f44b669b0c382
Alijon Karimberdiev <coding802@gmail.com>
2024-08-17 01:46:17 -0500

    testing schema of get and put requests

--- app/backend/core2/serializers.py
+++ app/backend/core2/serializers.py
@@ -21,22 +21,31 @@ class TutorialSerializer(serializers.ModelSerializer):
         model = Tutorial
         fields = '__all__'
 
+class SectionProgressSerializer(serializers.ModelSerializer):
+    completed = serializers.BooleanField(source='progress.completed')
 
-class SimplifiedSectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Section
-        fields = ['id', 'slug', 'title'] 
+        fields = ['id', 'slug', 'title', 'completed']
 
-class SimplifiedChapterSerializer(serializers.ModelSerializer):
+class ChapterProgressSerializer(serializers.ModelSerializer):
+    sections = SectionProgressSerializer(many=True)
+    progress = serializers.SerializerMethodField()
+
     class Meta:
         model = Chapter
-        fields = ['id', 'title'] 
+        fields = ['id', 'title', 'sections', 'progress']
 
+    def get_progress(self, obj):
+        total_sections = obj.sections.count()
+        completed_sections = obj.sections.filter(progress__completed=True).count()
+        if total_sections > 0:
+            return (completed_sections / total_sections) * 100
+        return 0
 
 class UserProgressSerializer(serializers.ModelSerializer):
-    section = SimplifiedSectionSerializer()
-    chapter = SimplifiedChapterSerializer()
+    chapter = ChapterProgressSerializer(source='section.chapter')
 
     class Meta:
         model = UserProgress
-        fields = ['id', 'user', 'section', 'chapter', 'completed', 'progress', 'last_accessed']
+        fields = ['id', 'chapter']
