2aa713f53692e9dc18f64248515a596696487b51
Alijon Karimberdiev <coding802@gmail.com>
2024-08-17 02:17:05 -0500

    Fixing progress bar on study space navbar

--- app/frontend/src/components/Dashboard/utils/studyspace/StudySpaceBody.tsx
+++ app/frontend/src/components/Dashboard/utils/studyspace/StudySpaceBody.tsx
@@ -2,23 +2,17 @@ import React, { useState, useRef, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { useNavigate, useParams } from 'react-router-dom';
 import { getSection } from '../../../auth_utils/axios/axiosServices';
+import { Section } from '../../../auth_utils/types';
 
-interface Section {
-  id: number;
-  slug: string;
-  title: string;
-  description: string;
-  content: string;
-  chapter: number;
-}
 
 interface StudySpaceBodyProps {
   isDarkMode: boolean;
+  onProgressChange: (currentStep: number) => void;
 }
 
-const chunkSize = 1; 
+const chunkSize = 1;
 
-const StudySpaceBody: React.FC<StudySpaceBodyProps> = ({ isDarkMode }) => {
+const StudySpaceBody: React.FC<StudySpaceBodyProps> = ({ isDarkMode, onProgressChange }) => {
   const { sectionSlug } = useParams<{ sectionSlug: string }>();
   const navigate = useNavigate();
   const darkModeClass = isDarkMode ? 'text-slate-300 bg-gray-900' : 'bg-white text-gray-600';
@@ -49,12 +43,17 @@ const StudySpaceBody: React.FC<StudySpaceBodyProps> = ({ isDarkMode }) => {
     fetchSectionData();
   }, [sectionSlug]);
 
+  useEffect(() => {
+    const totalParagraphs = section?.content.split('\n').filter(paragraph => paragraph.trim() !== '').length || 0;
+    const calculatedStep = Math.ceil((visibleChunks / totalParagraphs) * 5);
+    onProgressChange(calculatedStep);
+  }, [visibleChunks, section, onProgressChange]);
+
   const handleLoadMore = () => {
-    setVisibleChunks(prev => Math.min(prev + 1, Math.ceil(paragraphs.length / chunkSize)));
+    setVisibleChunks(prev => Math.min(prev + 1, paragraphs.length));
   };
 
   const handleFinish = () => {
-    // Navigate back to the course path or wherever you'd like after finishing.
     if (section?.chapter) {
       navigate(`/demo/tutorials/course-paths/${section.chapter}`);
     } else {
@@ -76,10 +75,6 @@ const StudySpaceBody: React.FC<StudySpaceBodyProps> = ({ isDarkMode }) => {
     }
   }, [visibleChunks]);
 
-  if (loading) return <div className="text-center p-4">Loading...</div>;
-  if (error) return <div className="text-center p-4 text-red-500">{error}</div>;
-
-  // Split the section content into paragraphs
   const paragraphs = section?.content.split('\n').filter(paragraph => paragraph.trim() !== '') || [];
   const currentContent = paragraphs.slice(0, visibleChunks * chunkSize);
 
@@ -89,33 +84,33 @@ const StudySpaceBody: React.FC<StudySpaceBodyProps> = ({ isDarkMode }) => {
     <div className={`fixed h-screen ml-[30%] w-[70%] ${darkModeClass} hidden md:block`}>
       <div className='flex flex-col h-full'>
         <div ref={scrollContainerRef} className='flex-grow overflow-y-auto space-y-4 mb-16 mt-10 px-20 py-20'>
-            <AnimatePresence>
-                {currentContent.map((paragraph, index) => (
-                    <motion.div
-                    key={index}
-                    initial={{ opacity: 0, y: 10 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    exit={{ opacity: 0, y: 10 }}
-                    transition={{ duration: 0.5 }}
-                    >
-                    <p className="mb-6 text-lg font-semibold leading-relaxed">
-                        {paragraph}
-                    </p>
-                    {index < currentContent.length - 1 && (
-                        <div className="flex justify-center items-center my-14">
-                        <SmallWavyDivider isDarkMode={isDarkMode} />
-                        </div>
-                    )}
-                    </motion.div>
-                ))}
-            </AnimatePresence>
+          <AnimatePresence>
+            {currentContent.map((paragraph, index) => (
+              <motion.div
+                key={index}
+                initial={{ opacity: 0, y: 10 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: 10 }}
+                transition={{ duration: 0.5 }}
+              >
+                <p className="mb-6 text-lg font-semibold leading-relaxed">
+                  {paragraph}
+                </p>
+                {index < currentContent.length - 1 && (
+                  <div className="flex justify-center items-center my-14">
+                    <SmallWavyDivider isDarkMode={isDarkMode} />
+                  </div>
+                )}
+              </motion.div>
+            ))}
+          </AnimatePresence>
           <div ref={contentEndRef} />
         </div>
         <div className="fixed bottom-3 right-5">
           <AnimatePresence>
             {isAllContentVisible && (
               <motion.button
-                key="finishButton" // Ensure a unique key for animation
+                key="finishButton"
                 onClick={handleFinish}
                 className={`bg-blue-600 w-auto p-2 sm:px-6 border-[1px] rounded-xl font-semibold text-base ${isDarkMode ? 'hover:bg-blue-800' : 'hover:bg-blue-800 text-white'}`}
                 initial={{ opacity: 0, scale: 0.8 }}
@@ -128,7 +123,7 @@ const StudySpaceBody: React.FC<StudySpaceBodyProps> = ({ isDarkMode }) => {
             )}
             {!isAllContentVisible && (
               <motion.button
-                key="continueButton" // Ensure a unique key for animation
+                key="continueButton"
                 onClick={handleLoadMore}
                 className={`bg-blue-600 w-auto p-2 sm:px-6 border-[1px] rounded-xl font-semibold text-base ${isDarkMode ? 'hover:bg-blue-800' : 'hover:bg-blue-800 text-white'}`}
                 initial={{ opacity: 0, scale: 0.8 }}
@@ -147,8 +142,6 @@ const StudySpaceBody: React.FC<StudySpaceBodyProps> = ({ isDarkMode }) => {
 };
 
 const SmallWavyDivider: React.FC<{ isDarkMode: boolean }> = ({ isDarkMode }) => {
-  const dividerColor = isDarkMode ? '#ffffff' : '#4B5563';
-
   return (
     <div className='flex flex-row items-center justify-center gap-4'>
       <div className='py-[1.5px] px-4 bg-blue-600 rounded-2xl'/>
